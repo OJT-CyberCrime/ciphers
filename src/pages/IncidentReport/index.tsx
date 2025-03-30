82081883eab1a2cdf39441dd02338bcdf920f595
Lorenzo Jay R. Pantalla <168610844+ljpantalla@users.noreply.github.com>
2025-03-30 15:54:42 +0800

    Update incident.tsx

--- src/pages/IncidentReport/index.tsx
+++ src/pages/IncidentReport/index.tsx
@@ -111,10 +111,12 @@ export default function IncidentReport() {
   const previousPageName = "Home";
   const [sortCriteria, setSortCriteria] = useState("created_at");
 
-  const userRole = JSON.parse(Cookies.get('user_data') || '{}').role;
+  const userRole = JSON.parse(Cookies.get("user_data") || "{}").role;
 
   const canEditOrArchive = () => {
-    return userRole === 'admin' || userRole === 'superadmin' || userRole === 'wcpd';
+    return (
+      userRole === "admin" || userRole === "superadmin" || userRole === "wcpd"
+    );
   };
 
   // Fetch folders with their categories from Supabase
@@ -545,14 +547,14 @@ export default function IncidentReport() {
                 ))
               ) : (
                 <div className="flex flex-col items-center justify-center h-full text-gray-500 py-8 font-poppins">
-                <DotLottieReact
-                  src="/assets/NoFiles.lottie"
-                  loop
-                  autoplay
-                  className="w-6/12"
-                />
-                No files found in this folder
-              </div>
+                  <DotLottieReact
+                    src="/assets/NoFiles.lottie"
+                    loop
+                    autoplay
+                    className="w-6/12"
+                  />
+                  No files found in this folder
+                </div>
               )}
             </tbody>
           </table>
@@ -702,15 +704,16 @@ export default function IncidentReport() {
               </div>
             ))
           ) : (
-    // Fix: Ensuring Lottie is centered by spanning all columns
-    <div className="col-span-full flex flex-col items-center justify-center h-[50vh] text-gray-500 font-poppins">
-        <DotLottieReact
-          src="/assets/NoFiles.lottie"
-          loop
-          autoplay
-          className="w-6/12"
-        />No files found in this folder
-    </div>
+            // Fix: Ensuring Lottie is centered by spanning all columns
+            <div className="col-span-full flex flex-col items-center justify-center h-[50vh] text-gray-500 font-poppins">
+              <DotLottieReact
+                src="/assets/NoFiles.lottie"
+                loop
+                autoplay
+                className="w-6/12"
+              />
+              No files found in this folder
+            </div>
           )}
         </div>
       )}
@@ -729,7 +732,7 @@ export default function IncidentReport() {
         availableCategories={availableCategories}
       />
 
-      <PermissionDialog 
+      <PermissionDialog
         isOpen={showPermissionDialog}
         onClose={() => setShowPermissionDialog(false)}
         action={permissionAction}
